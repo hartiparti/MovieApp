820b996bf047f031e1c2298c2377002173ef8137
hartiparti <34035433+hartiparti@users.noreply.github.com>
2018-03-22 13:43:25 +0000

    Stable build
    Uppfaert

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -5,15 +5,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MovieApp.Models;
+using MovieApp.Models.ViewModels;
+using MovieApp.Models.InputModels;
+using MovieApp.Models.EntityModels;
 
 namespace MovieApp.Controllers
 {
     public class ActorsController : Controller
     {   
         [HttpGet]
+  
         public IActionResult Index()
-        {
-            return View();
+        {   
+            var a_db = (from a in FakeDatabase.Actors
+                        select new ActorViewModel
+                         {
+                             Id = a.Id,
+                             Name = a.Name,
+                             Age = a.Age,
+
+                         }).ToList();
+            
+            return View(a_db);
         }
         [HttpGet]
         public IActionResult Error()
